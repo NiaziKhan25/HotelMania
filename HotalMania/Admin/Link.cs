2d0393a5cd9c6e4a28970d4414e0f27284ef32fd
Niazi. Khan <Niazi. Khan@NIAZI-PC>
2017-11-21 21:56:31 +0500

    Link

---
+++ HotalMania/Admin/Link.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotalMania.Admin
+{
+    public class Link
+    {
+        /*https://www.youtube.com/watch?v=LPsyTf6EK5U*/
+    }
+}
