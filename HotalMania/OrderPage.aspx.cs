a032fe0d64ca11762df8029be9a747e85ef41cc4
awais <awais00094@gmail.com>
2017-11-29 20:50:09 +0500

    commet form awais order page

---
+++ HotalMania/OrderPage.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HotalMania
+{
+    public partial class OrderPage : System.Web.UI.Page
+    {
+        int hotalID;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            hotalID = Int32.Parse(Request.QueryString["ID"]);
+        }
+    }
+}
---
+++ HotalMania/Scripts/customer.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.datepicker').pickadate();
+    $('#input_starttime').pickatime({
+        twelvehour: true
+    });
+    $('#input_endtime   ').pickatime({
+        twelvehour: true
+    });
+
+});
